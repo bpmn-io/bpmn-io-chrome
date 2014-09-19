6c9d0187cd53902f0ac00e51a0de2fd89df64d72
Sebastian Menski <sebastian.menski@googlemail.com>
2014-09-19 14:41:45 +0200

    chore app dimension

--- app/main.js
+++ app/main.js
@@ -2,16 +2,14 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
-  var width = 500;
-  var height = 300;
+  var width = 1024;
+  var height = 768;
 
   chrome.app.window.create('index.html', {
     id: "bpmn-js-chrome",
-    outerBounds: {
+    innerBounds: {
       width: width,
       height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
     }
   }, function(win) {
     win.contentWindow.launchData = launchData;
