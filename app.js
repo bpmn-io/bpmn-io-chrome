29084b8761d6821800fac1de813af5e9e53dba4f
Sebastian Menski <sebastian.menski@googlemail.com>
2014-09-19 11:29:31 +0200

    add a big ball of mud

---
+++ app.js
@@ -0,0 +1,26 @@
+chrome.app.runtime.onLaunched.addListener(function (launchData) {
+    chrome.app.window.create('index.html', {}, function (win) {
+        win.contentWindow.launchData = launchData;
+    });
+});
+
+
+
+var BpmnViewer = window.BpmnJS;
+
+
+var viewer = new BpmnViewer({ container: '#canvas', height: '100%', width: '100%' });
+
+
+//$.get('pizza-collaboration.bpmn', function(pizzaDiagram) {
+//
+//  viewer.importXML(pizzaDiagram, function(err) {
+//
+//    if (!err) {
+//      console.log('success!');
+//      viewer.get('canvas').zoom('fit-viewport');
+//    } else {
+//      console.log('something went wrong:', err);
+//    }
+//  });
+//});
