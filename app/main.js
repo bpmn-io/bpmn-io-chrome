1a48bccc5c16190379393de82d032927ea79ffc7
Sebastian Menski <sebastian.menski@googlemail.com>
2014-09-19 14:14:30 +0200

    merge background.js and main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,4 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- * @see http://developer.chrome.com/apps/app.window.html
- */
-chrome.app.runtime.onLaunched.addListener(function() {
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
   // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
@@ -19,5 +13,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
       left: Math.round((screenWidth-width)/2),
       top: Math.round((screenHeight-height)/2)
     }
+  }, function(win) {
+    win.contentWindow.launchData = launchData;
   });
+
 });
